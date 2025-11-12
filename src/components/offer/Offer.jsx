309f0669bcf6549162202709f0c30d8ca08b6d03
Michal Karbowniczek <michaldkarbowniczek@gmail.com>
2025-11-12 22:25:07 +0100

    Added responisvnes of the website

--- src/components/offer/Offer.jsx
+++ src/components/offer/Offer.jsx
@@ -8,15 +8,27 @@ const Offer = () => {
         <h2>Oferta</h2>
 
         <div className="offer-block">
-          <h3>JEDNA OSOBA</h3>
-          <p>120 zł / 60 min</p>
+          <h3>lekcje indywidualne</h3>
           <p>95 zł / 45 min</p>
+          <p>120 zł / 60 min</p>
         </div>
 
         <div className="offer-block">
-          <h3>DWIE OSOBY</h3>
-          <p>(znajomi, rodzeństwo itp. na podobnym poziomie)</p>
+          <h3>lekcje grupowe</h3>
           <p>70 zł / 60 min / os.</p>
+        </div>
+
+        <div className="offer-block">
+            <h3>tłumaczenie zwykłe ENG-PL</h3>
+            <p>35 zł / 1 str. (1500 znaków ze spacjami)</p>
+        </div>
+
+        <div className="offer-block">
+           <h3>korekta tłumaczenia przeprowadzona przez inny podmiot</h3>
+           <p>15 zł / 1 str. (1500 znaków ze spacjami)</p>
+        </div>
+        <div className="offer-block">
+            <p>Możliwość wystawienia faktury VAT.</p>
         </div>
       </div>
     </section>
