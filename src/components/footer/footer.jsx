975f4a4e913012b317e74c14c6fd9c040d082d68
Michal Karbowniczek <michaldkarbowniczek@gmail.com>
2025-11-13 22:03:30 +0100

    Typo fix

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -4,7 +4,7 @@ import "./Footer.scss";
 const Footer = () => {
   return (
     <footer className="footer-section">
-      <p>© 2025 Anna Naglarska – Wszystkie prawa zastrzeżone</p>
+      <p>© 2025 Aneta Naglarska – Wszystkie prawa zastrzeżone</p>
     </footer>
   );
 };
