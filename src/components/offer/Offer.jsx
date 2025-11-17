ef020cc6dbc9db5917328aac4dfaacd93b834ebd
Michal Karbowniczek <michaldkarbowniczek@gmail.com>
2025-11-17 18:21:41 +0100

    updated offer prices

--- src/components/offer/Offer.jsx
+++ src/components/offer/Offer.jsx
@@ -19,17 +19,20 @@ const Offer = () => {
         <div className="offer-items">
           <div className="offer-item">
             <h3>Zajęcia indywidualne</h3>
-            <p>95 zł / 45 min</p>
-            <p>120 zł / 60 min</p>
-            <h3>Zajęcia grupowe</h3>
-            <p>80 zł / 60 min / os.</p>
+            <p>120 zł / 55 min</p>
+            <h3>Pakiet 10 zajęć*</h3>
+             <p>1100zł</p>
+            <p>*do wykorzystania w ciągu 12 tygodni przy zajęciach raz w tygodniu</p>
+            <p>*do wykorzystania w ciągu 7 tygodni przy zajęciach dwa razy w tygodniu</p>
+            <h3>Zajęcia w parach</h3>
+            <p>80 zł / 55 min / os.</p>
           </div>
 
           <div className="offer-item">
-            <h3>Tłumaczenie zwykłe ENG–PL</h3>
-            <p>35 zł / 1 str. (1500 znaków ze spacjami)</p>
-            <h3>Korekta tłumaczenia</h3>
-            <p>15 zł / 1 str. (1500 znaków ze spacjami)</p>
+            <h3>Tłumaczenia ENG–PL</h3>
+            <p>40 zł / 1 str. (1500 znaków ze spacjami)</p>
+            <h3>Tłumaczenia PL-ENG</h3>
+            <p>45 zł / 1 str. (1500 znaków ze spacjami)</p>
           </div>
 
           <div className="offer-item">
